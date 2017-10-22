6a9067bdedca45bf9632c106da86b62d40399cfa
Lincy <linchangyuan1996@gmail.com>
2017-10-23 00:44:07 +0800

    Retrieve Setting
    T-C-f Sweep 实验设置读取
    修正UI中的命名错误

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
 import sys
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QMessageBox
 from PyQt5.QtCore import Qt
 from ui_mainwindow import Ui_MainWindow
+import visa
+rm = visa.ResourceManager()
 
 class main(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+
     def connect(self):
-        print('a')
+        if len(rm.list_resources())==0:
+            print('No Device Found.')
+        else:
+            print('Device Found:', rm.list_resources())
+        try:
+            cm22c = rm.open_resource('GPIB0::12::INSTR')
+            print("CM22C Successfully Connected")
+            lcr4284a = rm.open_resource('GPIB0::25::INSTR')
+            print("LCR4284A Successfully Connected")
+            QMessageBox.information(self,"Connect Instruments","Connected instruemnts successfully.",QMessageBox.Ok|QMessageBox.Ok)
+        except:
+            print('Connection Error')
+            QMessageBox.critical(self,"Connect Instruments","Fail to connect relevant instruments. Please check the connection.",QMessageBox.Ok|QMessageBox.Ok)
+    
+    def startTCf(self):
+        print('Start T-C-f Sweep.')
+        StartT=self.boxStartT.text()
+        StopT=self.boxStopT.text()
+        StepT=self.boxStepT.text()
+        Startf=self.boxStartf.text()
+        Stopf=self.boxStopf.text()
+        Multiplef=self.boxMultiplef.text()
+        LevelV=self.boxLevelV.text()
+        BiasV=self.boxBiasV.text()        
+        if self.rButtonSHORT.isChecked():
+            strIntegration='SHOR,'
+        elif self.rButtonMEDIUM.isChecked():
+            strIntegration='MED,'
+        elif self.rButtonLONG.isChecked():
+            strIntegration='LONG,'
+        print(StartT,StopT,StepT,Startf,Stopf,Multiplef,LevelV,BiasV,strIntegration)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
